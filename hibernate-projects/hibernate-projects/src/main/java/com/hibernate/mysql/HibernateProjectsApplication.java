a742f07a5e63d0fe8362fb425e7d5188dbf1219b
Mahesh Bandarupalli <mahesh10205@gmail.com>
2020-09-07 20:16:11 -0400

    first -commit

---
+++ hibernate-projects/hibernate-projects/src/main/java/com/hibernate/mysql/HibernateProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateProjectsApplication.class, args);
+	}
+
+}
---
+++ hibernate-projects/hibernate-projects/src/test/java/com/hibernate/mysql/HibernateProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernate.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
